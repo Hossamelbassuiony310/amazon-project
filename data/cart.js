81439dbc1b7ef803974858448daaae621d10150f
Hossam El-Sayed El-Bassuiony <hossambesso7@gmail.com>
2025-09-20 02:25:58 +0300

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,11 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem("cart")) || [
   { productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", quantity: 2 },
   { productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d", quantity: 1 },
 ];
 
+export function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 export function addToCart(productId, selectedQuantity) {
   let matchingItem = cart.find((item) => item.productId === productId);
 
@@ -11,6 +14,7 @@ export function addToCart(productId, selectedQuantity) {
   } else {
     cart.push({ productId, quantity: selectedQuantity });
   }
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -22,4 +26,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
