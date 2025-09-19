915baf67e2ead0e9512e97ff601bb65e20164e5c
Hossam El-Sayed El-Bassuiony <hossambesso7@gmail.com>
2025-09-19 22:53:14 +0300

    use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,5 @@
 console.log("This is amazon.js file");
 
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: { stars: 4.5, count: 87 },
-    priceCents: 1090,
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: { stars: 4, count: 127 },
-    priceCents: 2095,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: { stars: 4.5, count: 56 },
-    priceCents: 799,
-  },
-  {
-    image: "images/products/men-navigator-sunglasses-brown.jpg",
-    name: "Men Navigator Sunglasses Brown",
-    rating: { stars: 4, count: 60 },
-    priceCents: 1000,
-  },
-];
-
 let productsHTML = "";
 
 products.forEach((product) => {
@@ -77,7 +50,5 @@ products.forEach((product) => {
     </div>
   `;
 });
-
-console.log(productsHTML);
 
 document.querySelector(".js-product-list").innerHTML = productsHTML;
